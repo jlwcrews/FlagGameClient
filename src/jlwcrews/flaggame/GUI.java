041ee3e668bc5bee76ea921d0a2ab8a25a17d1b5
jlwcrews <jlwcrews@gmail.com>
2018-04-18 01:36:27 +0200

    Basic game working

---
+++ src/jlwcrews/flaggame/GUI.java
@@ -0,0 +1,14 @@
+package jlwcrews.flaggame;
+
+public class GUI {
+
+    private static String difficulty;
+
+    public static String getDifficulty() {
+        return difficulty;
+    }
+
+    public static void setDifficulty(String difficulty) {
+        GUI.difficulty = difficulty;
+    }
+}
